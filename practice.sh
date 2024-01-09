843f8d04e7664e65ed27fce8436f654c0561c535
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2024-01-09 09:42:25 -0700

    automate with sh

---
+++ practice.sh
@@ -0,0 +1,26 @@
+cd ~/OneDrive/Desktop/mtechjan24/jan09/jan09
+mkdir Projects/
+cd Projects
+mkdir SampleProject/
+cd SampleProject/
+touch index.html
+mkdir css/ pages/ contact/ images/ js/
+cd css/
+touch style.css
+cd ..
+cd pages/
+mkdir about/
+cd about/
+touch about.html about.css
+cd ../../
+cd contact/
+touch contact.html contact.css
+cd ..
+cd images/
+touch companyLogo.png
+mkdir icons/
+cd icons/
+touch email.png phone.png chat.png
+cd ../../
+cd js/
+touch index.js
